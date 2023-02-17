802daedaa63442ecd77ea455150eb66ced1cbc3f
Jhansi <96642740+jhanu656@users.noreply.github.com>
2023-02-17 17:18:04 +0530

    STRING
    Write a program to change the given string to lowercase without using string library functions: 
    
    INPUT & OUTPUT FORMAT: 
    
    Input consists of 1 string. 
    
    Output print the Lowercase of given string.
    
    Sample Input:
    CYFUNO
    
    Sample Output:
    
    cyfuno

---
+++ String8.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class String8
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new  Scanner(System.in);
+        String a=sc.next();
+        String c=a.toLowerCase();
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
