6cf5fb91d6e0f76a3f733b8c2f70b6c2c1ebd7a4
Jhansi <96642740+jhanu656@users.noreply.github.com>
2023-02-17 19:42:25 +0530

    L2
    Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
    
    Sample input: 
    
    1,2,3,4,5,6,7,8,9
    
    Sample Output: 
    
    1,3,5,7,9

---
+++ L2odd.java
@@ -0,0 +1,21 @@
+//Code here
+import java.io.*;
+import java.util.*;
+public class odd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[100];
+        int k=0;
+        for(int i=0;i<30;i++)
+        {
+            a[i]=sc.next();
+            if(a[i]==" ")
+            {
+                break;
+            }
+            k++;
+        }
+    }
+}
