10f6bcce375f3400eecb47259c8daa14411f45f5
Jhansi <96642740+jhanu656@users.noreply.github.com>
2023-02-17 16:55:43 +0530

    2-Dimensional Array
    Write a C program to find the concatenation of given two strings using string library functions:
    
    INPUT & OUTPUT FORMAT: 
    
    Input consists of 2 strings.
    
    Output consists of concatenated strings
    
    Sample Input:
    cyfuno
    
    technology
    
    Sample Output:
    
    cyfunotechnology

---
+++ String6.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class String6
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new  Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        String c=a.concat(b);
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
