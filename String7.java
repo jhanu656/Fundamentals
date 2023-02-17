635a4bfd667af5d9b3e05fd0c3f9a61a59e0048f
Jhansi <96642740+jhanu656@users.noreply.github.com>
2023-02-17 17:08:47 +0530

    2-Dimensional Array
    Write a program to change the given string to uppercase without using string library functions: 
    
     INPUT & OUTPUT FORMAT: 
    
    Input consists of 1 string. 
    
    Output print the Uppercase of given string.
    
    Sample Input:
    cyfuno
    
    Sample Output:
    
    CYFUNO

---
+++ String7.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class String7
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new  Scanner(System.in);
+        String a=sc.next();
+        String c=a.toUpperCase();
+        System.out.println(c);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
