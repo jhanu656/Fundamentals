d632f454d513dde145c50a82b31f8dbc0f3846d7
Jhansi <96642740+jhanu656@users.noreply.github.com>
2023-02-17 16:44:12 +0530

    2-Dimensional Array
    Write a program to find whether the given two strings are same or not using string library functions:
    
    INPUT & OUTPUT FORMAT:
    
    Input consists of 2 strings. 
    
    If two strings are same, display “Strings are same”, else display “Strings are not same”.
    
    Sample Input:
    hello 
    
    hello 
    
    Sample Output:
    Strings are same

---
+++ String5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class String5
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new  Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        int c=b.compareTo(a);
+        if (c==0)
+        System.out.println("Strings are same");
+        else
+        System.out.println("Strings are not same");
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
